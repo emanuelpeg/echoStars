842273435f1fcbd515676214f7f936902e6f8a95
Axel Salmeron <axul@users.noreply.github.com>
2023-10-19 10:20:45 -0500

    Adds local NodeJs Express server for testing

---
+++ express-test-server/app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config()
+
+console.log(`NODE_ENV=${process.env.NODE_ENV}`);
+
+var indexRouter = require('./routes/index');
+const healthCheckRouter = require('./routes/health-check');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/healthCheck', healthCheckRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ express-test-server/readme.md
@@ -0,0 +1,9 @@
+# Express server
+
+Created using the [Express application generator](https://expressjs.com/en/starter/generator.html) running `npx express-generator` with node **v16.17.0**
+
+## Steps to run
+
+- Run `npm i`
+- Run `npm start`
+- Open the the [Local server in your browser](http://localhost:3000/)
